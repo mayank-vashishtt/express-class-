1235eb4bcd45a833089474b61639bdd256cb5ed4
mayank-vashishtt <mayankvashishtt@gmail.com>
2024-07-03 11:17:18 +0530

    custom made middleware

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
+app.use(middleware);
+
+// Logger middleware
+app.use((req, res, next) => {
+    const method = req.method;
+    const ip = req.ip;
+    const hostname = req.hostname;
+    const date = new Date().toISOString();
+
+    console.log(`[${date}] ${method} request from ${ip} to ${hostname}`);
+    next();
+});
 
 let courses = [
     { id: 1, name: "java" },
@@ -14,7 +26,7 @@ app.get('/courses', (req, res) => {
 });
 
 app.post('/courses', (req, res) => {
-    console.log(res.body);
+    console.log(req.body);
     let singleCourse = {
         id: courses.length + 1,
         name: req.body.name
@@ -56,6 +68,11 @@ app.delete('/course/:id', (req, res) => {
         res.status(500).send(err);
     }
 });
+
+function middleware(req, res, next) {
+    console.log("Middleware called");
+    next();
+}
 
 app.listen(3000, () => {
     console.log("server started on port 3000");
